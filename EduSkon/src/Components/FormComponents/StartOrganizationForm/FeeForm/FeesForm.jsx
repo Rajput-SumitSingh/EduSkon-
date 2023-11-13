382fdcdd60b63a12bc109833de48ffe826954ea1
Sumit Singh <103952482+Rajput-SumitSingh@users.noreply.github.com>
2023-11-14 01:04:18 +0530

    Delete EduSkon directory

--- EduSkon/src/Components/FormComponents/StartOrganizationForm/FeeForm/FeesForm.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function FeesForm() {
-  return (
-    <div>FeesForm</div>
-  )
-}
-
-export default FeesForm
--- EduSkon/src/Components/FormComponents/StartOrganizationForm/OrganizationForm/OrganizationForm.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function OrganizationForm() {
-  return (
-    <div>OrganizationForm</div>
-  )
-}
-
-export default OrganizationForm
